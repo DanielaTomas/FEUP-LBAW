dca0c4c8f9bd97aab514053d60127d9a56e13912
srissirs <sarareis2002@gmail.com>
2022-11-15 18:34:38 +0000

    added profile page, and invitation and event card components

---
+++ template-laravel/resources/views/partials/eventCard.blade.php
@@ -0,0 +1,24 @@
+<article class="eventcard" data-id=""> <!-- add event id-->
+    <section id="eventImage">
+        <div>
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+        <div>
+            <i class="fa-solid fa-bell"></i>
+            <i class="fa-solid fa-lock"></i>
+        </div>
+    </section>
+    <section id=eventCardLower>
+        <section id="eventCardLowerLeft">
+            <p id=eventCardDate> Start Date </p>
+            <p id=eventCardTime> Start Time </p>
+        </section>
+        <section id="eventCardLowerRight">
+            <p id=eventCardName> Name </p>
+            <div id=eventCardCategories> Sports </div>
+            <p id=eventCardDescription> Sports </p>
+            <p id=eventCardLocation> Location </p>
+            <div id=eventCardTags> tag </div>
+        </section>
+    </section>
+</article>
