d0b4cdf0bfcd3e5f0551c3ceefcc7ae1c95f3e51
srissirs <sarareis2002@gmail.com>
2022-11-15 16:45:59 +0000

    added navbar (without css)

--- template-laravel/resources/views/layouts/app.blade.php
+++ template-laravel/resources/views/layouts/app.blade.php
@@ -22,12 +22,7 @@
   </head>
   <body>
     <main>
-      <header>
-        <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
-        @if (Auth::check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-        @endif
-      </header>
+      @include('partials.navbar')
       <section id="content">
         @yield('content')
       </section>
