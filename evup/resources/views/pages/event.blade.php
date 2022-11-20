09dd2b66bf89bcf40a214861863914afa5168bd1
Daniela Tomás <daniela.stomas02@gmail.com>
2022-11-20 16:41:34 +0000

    update event page

--- evup/resources/views/pages/event.blade.php
+++ evup/resources/views/pages/event.blade.php
@@ -1,8 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div data-id="{{ $event->eventid }}" > {{ $event->eventname }} </div>
-    <article>
-    </article>
+<article class="eventcard" data-id="{{ $event->eventid }}">
+    <div class="flex flex-row p-5 items-center justify-between">
+        <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800">{{ $event->eventname }}</h1>
+        <button><i class="fa-solid fa-triangle-exclamation fa-2x"></i></button>
+    </div>
+    <section>
+    <section id="eventImage">
+        <div>
+            <button><i class="fa-solid fa-bell"></i></button>
+            <button><i class="fa-solid fa-lock"></i></button>
+        </div>
+    </section>
+    
+    <h2 class="mb-4 text-3xl leading-none tracking-tight text-gray-800">Description</h2>
+    <p id=eventCardDescription> {{ $event->description }} </p>
+    <div id=eventCardTags> @each('partials.tag', $event->eventTags()->get(), 'tag') </div>
+    </section>
+    <section id=eventCardLower>
+        <section id="eventCardLowerLeft">
+            <p id=eventCardStartDate> Start: {{ $event->startdate }} </p>
+            <p id=eventCardEndDate> End: {{ $event->enddate }} </p>
+        </section>
+        <section id="eventCardLowerRight">
+            <div id=eventCardCategories> @each('partials.category', $event->eventcategories()->get(), 'category') </div>
+            <p id=eventCardLocation> Address: {{ $event->address }} </p>
+        </section>
+    </section>
+</article>
+
 @endsection
 
