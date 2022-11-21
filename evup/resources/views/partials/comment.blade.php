69ef4f19a99ffa5397d2ac12a9763a9e935287e6
Daniela Tomás <daniela.stomas02@gmail.com>
2022-11-21 22:27:38 +0000

    finish comments

--- evup/resources/views/partials/comment.blade.php
+++ evup/resources/views/partials/comment.blade.php
@@ -1,1 +1,9 @@
-<p id = "comment" data-id="{{ $comment->commentid }}"> {{ $comment->commentcontent }}</p>
+
+<div id = "comment" data-id="{{ $comment->commentid }}"> 
+    <img class="w-6 h-6 rounded-full" src="https://randomuser.me/api/portraits/men/{{$comment->author()->first()->userid}}.jpg"/>
+
+    <p class="text-2xl font-bold leading-none tracking-tight text-gray-800">{{ $comment->author()->first()->username }}</p>
+
+    <p> {{ $comment->commentdate }} </p>
+    <p>{{ $comment->commentcontent }}</p>
+</div>
