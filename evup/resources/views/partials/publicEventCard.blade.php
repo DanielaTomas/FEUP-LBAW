460a71fc691bf3ab671e652f276198b46de8f5f8
srissirs <sarareis2002@gmail.com>
2022-11-23 19:45:14 +0000

    fixes

--- evup/resources/views/partials/publicEventCard.blade.php
+++ evup/resources/views/partials/publicEventCard.blade.php
@@ -5,12 +5,10 @@
     <div class="flex flex-col w-full md:flex-row">
         <div
             class="flex flex-row justify-around p-4 font-bold leading-none text-gray-800 uppercase bg-gray-400 rounded md:flex-col md:items-center md:justify-center md:w-1/4">
-            <div class="md:text-3xl">Jan</div>
-            <div class="md:text-6xl">13</div>
-            <div class="md:text-xl">7 pm</div>
+            <div class="md:text-xl">{{ $event->startdate }}</div>
         </div>
         <div class="p-4 font-normal text-gray-800 md:w-3/4">
-            <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800">{{ $event->eventname }}</h1>
+            <a href="event/{{$event->eventid}}"><h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800">{{ $event->eventname }}</h1></a>
             <div id=eventCardCategories> @each('partials.category', $event->eventcategories()->get(), 'category') </div>
             <p class="leading-normal">{{ $event->description }}</p>
             <div class="flex flex-column items-center mt-4 ">
