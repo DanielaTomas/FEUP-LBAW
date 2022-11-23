935a4c4396f206404fa3c977de58b6713c2970bc
srissirs <sarareis2002@gmail.com>
2022-11-23 17:23:23 +0000

    fix

--- evup/resources/views/partials/invitation.blade.php
+++ evup/resources/views/partials/invitation.blade.php
@@ -8,12 +8,12 @@
 								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
 									<div class="flex items-center">
 											<div class="ml-3">
-                                                @each('partials.eventname', $invite->event()->get(), 'event')
+                                                @each('partials.eventName', $invite->event()->get(), 'event')
 											</div>
 										</div>
 								</td>
 								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-								@each('partials.username', $invite->sender()->get(), 'user')	
+								@each('partials.userName', $invite->sender()->get(), 'user')	
         
 								</td>
                                 @if (Auth::check()) <!-- if status == null-->
