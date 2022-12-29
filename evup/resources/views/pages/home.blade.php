d3fa8eb5d3bbce4b05e518a73d1772d9d1e3e540
José Miguel Isidro <up202006485@up.pt>
2022-12-29 16:52:08 +0000

    added evup logo in login and register pages

--- evup/resources/views/pages/home.blade.php
+++ evup/resources/views/pages/home.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 
-@if(Session::has('success'))
-<div class="flex p-4 mb-4 bg-green-100 rounded-lg dark:bg-green-200"> <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-green-700 dark:text-green-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
-    </svg> <span class="sr-only">Info</span>
-    <div class="ml-3 text-sm font-medium text-green-700 dark:text-green-800"><span class="font-medium">Success!</span> {{ Session::get('success') }} </div>
-</div>
-@endif
-
 <section id="homeTop" class="flex justify-between">
 
     <div class="flex-auto max-w-xl mb-4">
