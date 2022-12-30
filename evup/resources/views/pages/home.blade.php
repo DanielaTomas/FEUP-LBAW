9bba427991702ae5c7e172acf2587db52f0fb5be
DanielaTomas <up202004946@edu.fc.up.pt>
2022-12-30 17:46:36 +0000

    fix home page css

--- evup/resources/views/pages/home.blade.php
+++ evup/resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
 
     <div class="flex-auto max-w-xl mb-4">
         <form id="searchForm" action="{{ route('search') }}">
-            <label for="publicSearch" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search Events</label>
+            <label for="event-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search Events</label>
             <div class="relative">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                     <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -29,16 +29,16 @@
     </div>
 
 </div>
-<div class="rounded-t-3xl">
+<article class="rounded-t-3xl">
     <div id="homeTagsSection">
         <p id="homeTagHeader" class="pt-6 font-bold text-lg">Select your interests to get event suggestions based on what you love</p>
         <div id="homeTags">
             @each('partials.tag', $tags, 'tag')
         </div>
-</div>
+    </div>
     <div id="homeEvents">
         @include('partials.content.publicEvents', ['events' => $events])
-</div>
-</div>
+    </div>
+</article>
 
 @endsection
