a1b31d3196cf0c34700b9131dddc15f686edcbca
DanielaTomas <up202004946@edu.fc.up.pt>
2023-01-01 10:40:12 +0000

    add alt to some images

--- evup/resources/views/partials/attendee.blade.php
+++ evup/resources/views/partials/attendee.blade.php
@@ -2,7 +2,7 @@
     <td class="py-3 px-6 text-left">
         <div class="flex items-center">
             <div class="mr-2">
-                <img class="w-6 h-6 rounded-full" src="{{$attendee['user']->userphoto}}"/>
+                <img alt="" class="w-6 h-6 rounded-full" src="{{$attendee['user']->userphoto}}">
             </div>
             <p>{{$attendee['user']->name}}</p>
         </div>
