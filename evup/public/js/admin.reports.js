ba7e625caa7bf7f04b3daa47e354952913be3eb2
José Miguel Isidro <up202006485@up.pt>
2023-01-02 14:45:31 +0000

    fix duplicate ids in reports

--- evup/public/js/admin.reports.js
+++ evup/public/js/admin.reports.js
@@ -1,14 +1,14 @@
-function closeReport(id) {
-    const url = '/admin/reports/'+ id + '/close';
-    sendAjaxRequest('put', url, null, reportHandler(true, id));
+function closeReport(reportid) {
+    const url = '/admin/reports/'+ reportid + '/close';
+    sendAjaxRequest('put', url, null, reportHandler(true, reportid = reportid));
 }
 
-function delEvent(id) {
-    const url = '/admin/events/'+ id + '/delete';
-    sendAjaxRequest('put', url, null, reportHandler(false, id));
+function delEvent(reportid, eventid) {
+    const url = '/admin/events/'+ eventid + '/delete';
+    sendAjaxRequest('put', url, null, reportHandler(false, reportid, eventid));
 }
 
-function reportHandler(close, id) { // if close is true, act as a close report, else act as a delete event
+function reportHandler(close, reportid, eventid = null) { // if close is true, act as a close report, else act as a delete event
     if (this.status == 403) {
         window.location = '/login';
         return;
@@ -19,8 +19,8 @@ function reportHandler(close, id) { // if close is true, act as a close report, 
     empty = false
 
     if(close) {
-        closeDiv = select('#close-' + id).parentElement
-        reportstatus = select('#reportstatus-' + id)
+        closeDiv = select('#close-' + reportid).parentElement
+        reportstatus = select('#reportstatus-' + reportid)
         container = closeDiv.parentElement
 
         if (closeDiv.parentElement.children.length === 1) empty = true
@@ -30,10 +30,10 @@ function reportHandler(close, id) { // if close is true, act as a close report, 
         reportstatus.classList = 'bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs'
     }
     else {
-        confirmbtn = select('[onclick="delEvent(' + id + ')"]')
+        confirmbtn = select('[onclick="delEvent(' + reportid + ', ' + eventid + ')"]')
         realid = (confirmbtn.id).split('-')[1]
         delDiv = select('#del-' + realid).parentElement
-        eventstatus = select('#eventstatus-' + id)
+        eventstatus = select('#eventstatus-' + reportid + eventid)
         container = delDiv.parentElement
 
         if (delDiv.parentElement.children.length === 1) empty = true
