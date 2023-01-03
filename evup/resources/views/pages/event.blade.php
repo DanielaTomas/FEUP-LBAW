fec7d9b3ada3f9d9747d4fc55044456203afbc88
unknown <up202005388@fe.up.pt>
2023-01-03 16:09:37 +0000

    altered all buttons in events

--- evup/resources/views/pages/event.blade.php
+++ evup/resources/views/pages/event.blade.php
@@ -15,15 +15,15 @@
     <div class="flex flex-row items-center p-6">
         <h1 class=" text-4xl font-bold leading-none tracking-tight text-gray-800">{{ $event->eventname }}</h1>
         @if (Auth::check())
-            @if($event->userid == Auth::id())
-            <a href="{{ route('event_dashboard', ['id' => $event->eventid]) }}" title="Manage event details, view participants and review join requests" class="self-end flex items-center text-white m-4 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mr-2">
-                    <path fill-rule="evenodd" d="M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 01-.517.608 7.45 7.45 0 00-.478.198.798.798 0 01-.796-.064l-.453-.324a1.875 1.875 0 00-2.416.2l-.243.243a1.875 1.875 0 00-.2 2.416l.324.453a.798.798 0 01.064.796 7.448 7.448 0 00-.198.478.798.798 0 01-.608.517l-.55.092a1.875 1.875 0 00-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 01-.064.796l-.324.453a1.875 1.875 0 00.2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 01.796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 01.517-.608 7.52 7.52 0 00.478-.198.798.798 0 01.796.064l.453.324a1.875 1.875 0 002.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 01-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 001.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 01-.608-.517 7.507 7.507 0 00-.198-.478.798.798 0 01.064-.796l.324-.453a1.875 1.875 0 00-.2-2.416l-.243-.243a1.875 1.875 0 00-2.416-.2l-.453.324a.798.798 0 01-.796.064 7.462 7.462 0 00-.478-.198.798.798 0 01-.517-.608l-.091-.55a1.875 1.875 0 00-1.85-1.566h-.344zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clip-rule="evenodd" />
-                </svg> 
+        @if($event->userid == Auth::id())
+        <a href="{{ route('event_dashboard', ['id' => $event->eventid]) }}" title="Manage event details, view participants and review join requests" class="self-end flex items-center text-white m-4 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mr-2">
+                <path fill-rule="evenodd" d="M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 01-.517.608 7.45 7.45 0 00-.478.198.798.798 0 01-.796-.064l-.453-.324a1.875 1.875 0 00-2.416.2l-.243.243a1.875 1.875 0 00-.2 2.416l.324.453a.798.798 0 01.064.796 7.448 7.448 0 00-.198.478.798.798 0 01-.608.517l-.55.092a1.875 1.875 0 00-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 01-.064.796l-.324.453a1.875 1.875 0 00.2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 01.796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 01.517-.608 7.52 7.52 0 00.478-.198.798.798 0 01.796.064l.453.324a1.875 1.875 0 002.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 01-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 001.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 01-.608-.517 7.507 7.507 0 00-.198-.478.798.798 0 01.064-.796l.324-.453a1.875 1.875 0 00-.2-2.416l-.243-.243a1.875 1.875 0 00-2.416-.2l-.453.324a.798.798 0 01-.796.064 7.462 7.462 0 00-.478-.198.798.798 0 01-.517-.608l-.091-.55a1.875 1.875 0 00-1.85-1.566h-.344zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clip-rule="evenodd" />
+            </svg>
             Event Dashboard</a>
-            @elseif($event->userid != Auth::id() && Auth::user()->isAttending($event->eventid))
-            <a href="{{ route('attendees', ['id' => $event->eventid]) }}" class="self-end text-white m-4 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">View Participants</a>
-            @endif
+        @elseif($event->userid != Auth::id() && Auth::user()->isAttending($event->eventid))
+        <a href="{{ route('attendees', ['id' => $event->eventid]) }}" class="self-end text-white m-4 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">View Participants</a>
+        @endif
         @endif
         @if (!$event->eventcanceled)
         @auth
@@ -46,9 +46,11 @@
                 <div class="flex flex-col gap-4  rounded">
                     <div class=" h-64 bg-top bg-cover rounded-t flex flex-col  shadow-lg" style="background-image: url({{ asset('storage/images/image-'.$event->eventphoto.'.png')}})">
                         @if (!$event->public)
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8  mt-2 ml-2">
-                            <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z" clip-rule="evenodd" />
-                        </svg>
+                        <div class="self-start m-2" title="Private Event">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-8 h-8 p-1 rounded-lg bg-gray-900">
+                                <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
                         @endif
                     </div>
 
@@ -61,27 +63,27 @@
 
                     <div class="flex flex-row justify-around">
                         @if (Auth::check())
-                            @if (Auth::user()->joinRequests()->where('eventid', $event->eventid)->get()->count() == 0 && !Auth::user()->isAttending($event->eventid) && Auth::user()->usertype !== "Admin")
-                                <!-- Request to Join Event Modal toggle -->
-                                <button id="requestToJoinButton{{ $event->eventid }}" data-modal-toggle="staticModal-jr{{ $event->eventid }}" title="Request to join this event" class="items-center text-white m-4 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Request to join</button>
-                                @include('partials.join_request_modal', ['event' => $event])
-                            @endif
-                            @if (!$event->eventcanceled)
-                                @if (Auth::user()->isAttending($event->eventid))
-                                    @if ($event->organizer()->first()->userid !== $user->userid)
-                                        <!-- Leave Event Modal toggle -->
-                                        <button id="leaveEventButton{{ $event->eventid }}" data-modal-toggle="staticModal-le{{ $event->eventid }}" title="Leave this event" class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Leave Event</button>
-                                        @include('partials.leave_event_modal', ['event' => $event])
-                                    @else
-                                        <button id="leaveEventButton{{ $event->eventid }}" disabled title="You cannot leave your own event" class="items-center text-white right-2.5 bottom-2.5 bg-gray-600 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Leave Event</button>
-                                    @endif
-                                @endif
-                                @if (Auth::user()->usertype !== "Admin")
-                                    <!-- Report Event Modal toggle -->
-                                    <button id="reportEventButton{{ $event->eventid }}" data-modal-toggle="staticModal-re{{ $event->eventid }}" title="Report this event" class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Report Event</button>
-                                    @include('partials.report_event_modal', ['event' => $event])
-                                @endif
-                            @endif
+                        @if (Auth::user()->joinRequests()->where('eventid', $event->eventid)->get()->count() == 0 && !Auth::user()->isAttending($event->eventid) && Auth::user()->usertype !== "Admin")
+                        <!-- Request to Join Event Modal toggle -->
+                        <button id="requestToJoinButton{{ $event->eventid }}" data-modal-toggle="staticModal-jr{{ $event->eventid }}" title="Request to join this event" class="items-center text-white m-4 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Request to join</button>
+                        @include('partials.join_request_modal', ['event' => $event])
+                        @endif
+                        @if (!$event->eventcanceled)
+                        @if (Auth::user()->isAttending($event->eventid))
+                        @if ($event->organizer()->first()->userid !== $user->userid)
+                        <!-- Leave Event Modal toggle -->
+                        <button id="leaveEventButton{{ $event->eventid }}" data-modal-toggle="staticModal-le{{ $event->eventid }}" title="Leave this event" class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Leave Event</button>
+                        @include('partials.leave_event_modal', ['event' => $event])
+                        @else
+                        <button id="leaveEventButton{{ $event->eventid }}" disabled title="You cannot leave your own event" class="items-center text-white right-2.5 bottom-2.5 bg-gray-600 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Leave Event</button>
+                        @endif
+                        @endif
+                        @if (Auth::user()->usertype !== "Admin")
+                        <!-- Report Event Modal toggle -->
+                        <button id="reportEventButton{{ $event->eventid }}" data-modal-toggle="staticModal-re{{ $event->eventid }}" title="Report this event" class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700">Report Event</button>
+                        @include('partials.report_event_modal', ['event' => $event])
+                        @endif
+                        @endif
                         @endif
 
                     </div>
@@ -113,11 +115,11 @@
 
                 @if(($event->organizer()->first()->userid == $user->userid || Auth::user()->isAttending($event->eventid)) && Auth::user()->usertype !== "Admin")
                 <h2 class="text-3xl font-bold leading-none tracking-tight text-gray-800">Polls</h2><br>
-                    @if( $event->organizer()->first()->userid == $user->userid)
-                    <button id="CreatePoll" class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-bold py-2 px-4 rounded-lg">Create Poll</button>
-                    <div id="addhereform">
-                        <form method="post" class="mb-6" action="{{ route('create_poll', $event->eventid) }}">
-                        @csrf   
+                @if( $event->organizer()->first()->userid == $user->userid)
+                <button id="CreatePoll" class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-bold py-2 px-4 rounded-lg">Create Poll</button>
+                <div id="addhereform">
+                    <form method="post" class="mb-6" action="{{ route('create_poll', $event->eventid) }}">
+                        @csrf
                         <div class="rounded-lg bg-grey-500 mb-4">
                             <label class="block text-gray-700 text-sm font-bold mb-2">Question</label>
                             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="question" type="text" required>
@@ -128,39 +130,39 @@
                             <button id="addOption" type="button" class="items-center font-bold px-3 py-1 bg-gray-500 hover:bg-gray-900 hover:text-white transition ease-in-out duration-300 text-black rounded-lg">Add Option</button>
                         </div>
                         <button class="items-center text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 font-bold py-2 px-4 rounded-lg" type="submit">Create Poll</button>
-                        </form>
-                    </div>
-                    @endif
-                    <br>
-                    <div class="mb-4">
+                    </form>
+                </div>
+                @endif
+                <br>
+                <div class="mb-4">
                     @if($event->polls()->count()==0)
-                      <p class="text-center text-xl leading-none tracking-tight text-gray-800">No polls available</p>
+                    <p class="text-center text-xl leading-none tracking-tight text-gray-800">No polls available</p>
                     @endif
                     @if($event->organizer()->first()->userid == $user->userid || Auth::user()->isAttending($event->eventid))
-                        @each('partials.poll', $event->polls()->get(), 'poll')
+                    @each('partials.poll', $event->polls()->get(), 'poll')
                     @endif
-                    </div>
+                </div>
                 @endif
-                
+
                 @if($event->organizer()->first()->userid == $user->userid || Auth::user()->isAttending($event->eventid) || Auth::user()->usertype === "Admin")
                 <section>
                     <div class=" mx-auto ">
                         <div class="flex justify-between items-center mb-6">
                             <h2 class="text-3xl font-bold leading-none tracking-tight text-gray-800">Comments</h2>
                         </div>
-                            <div class="flex mx-auto items-center justify-center max-w-lg">
-                                @if (!$event->eventcanceled && Auth::user()->usertype !== "Admin")
-                                    <div class="flex flex-wrap -mx-3 mb-6">
-                                        <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Leave a comment</h2>
-                                        <div class="w-full md:w-full px-3 mb-2 mt-2">
-                                            <input id="commentTextArea" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-500 focus:outline-none focus:bg-white" type="text" name="commentcontent" placeholder="Type Your Comment" required>
-                                        </div>
-                                        <div class="w-full md:w-full flex items-start md:w-full px-3">
-                                            <button onclick="createNewComment({{ $event->eventid }})" class="items-center font-bold px-3 py-1 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 text-white rounded-lg">Post Comment</button>
-                                        </div>
-                                    </div>
-                                @endif
+                        <div class="flex mx-auto items-center justify-center max-w-lg">
+                            @if (!$event->eventcanceled && Auth::user()->usertype !== "Admin")
+                            <div class="flex flex-wrap -mx-3 mb-6">
+                                <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Leave a comment</h2>
+                                <div class="w-full md:w-full px-3 mb-2 mt-2">
+                                    <input id="commentTextArea" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-500 focus:outline-none focus:bg-white" type="text" name="commentcontent" placeholder="Type Your Comment" required>
+                                </div>
+                                <div class="w-full md:w-full flex items-start md:w-full px-3">
+                                    <button onclick="createNewComment({{ $event->eventid }})" class="items-center font-bold px-3 py-1 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 text-white rounded-lg">Post Comment</button>
+                                </div>
                             </div>
+                            @endif
+                        </div>
                         <section id="comments">
                             @each('partials.comment', $comments, 'comment')
                             {{$comments->links()}}
