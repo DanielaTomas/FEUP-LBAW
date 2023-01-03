b5ab4193966ea5be7bd24e2b6d4091f6b3aa2d85
unknown <up202005388@fe.up.pt>
2023-01-03 22:40:55 +0000

    changed docker_run

--- evup/docker_run.sh
+++ evup/docker_run.sh
@@ -6,6 +6,6 @@ env >> /var/www/.env
 php artisan clear-compiled
 php artisan config:clear
 php artisan storage:link
-php artisan migrate:refresh --seed
+php artisan migrate:refresh
 php-fpm8.1 -D
 nginx -g "daemon off;"
