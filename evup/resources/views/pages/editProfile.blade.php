e35a0b2f056f4c04a8fbf7d9b47965e20410209a
unknown <up202005388@fe.up.pt>
2023-01-03 17:58:28 +0000

    fixes in forms for accessibility

--- evup/resources/views/pages/editProfile.blade.php
+++ evup/resources/views/pages/editProfile.blade.php
@@ -3,21 +3,22 @@
 @section('title', '- Edit Profile')
 
 @section('content')
-<!-- component -->
-<h1>Edit My Information</h1>
-<br>
-<div class="flex items-center justify-center p-12">
+    <!-- component -->
+    <h1 class=" flex self-center text-lg font-bold">Edit My Information</h1>
+    <br>
+    <div class="flex items-center justify-center p-12">
 
-    <div class="mx-auto w-full max-w-[550px]">
-        <form method="POST" action="{{ route('editUser', ['id' => $user->userid]) }}" enctype="multipart/form-data">
-            @csrf
-            <div class="-mx-3 flex flex-wrap">
-                <div class="w-full px-3 sm:w-1/2">
-                    <div class="mb-5">
-                        <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
-                            First Name
-                        </label>
-                        <input required type="text" name="name" id="name" value="{{ $user->name }}" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
+        <div class="mx-auto w-full max-w-[550px]">
+            <form method="POST" action="{{ route('editUser', ['id' => $user->userid]) }}" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <div class="-mx-3 flex justify-center flex-wrap">
+                    <div class="w-full px-3 sm:w-1/2">
+                        <div class="mb-5">
+                            <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
+                                First Name
+                            </label>
+                            <input required type="text" name="name" id="name" value="{{ $user->name }}" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
+                        </div>
                     </div>
                     <div class="w-full px-3 sm:w-1/2">
                         <div class="mb-5">
@@ -30,7 +31,7 @@
                     <div class="w-full px-3 sm:w-1/2">
                         <div class="mb-5">
                             <label for="username" class="mb-3 block text-base font-medium text-[#07074D]">
-                                Username
+                                New Username
                             </label>
                             <input required type="text" name="username" id="username" value="{{ $user->username }}" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
                         </div>
@@ -38,53 +39,21 @@
                     <div class="w-full px-3 sm:w-1/2">
                         <div class="mb-5">
                             <label for="username" class="mb-3 block text-base font-medium text-[#07074D]">
-                                Profile Photo
+                                New Profile Photo
                             </label>
-                            <input class="rounded-md bg-gray-300" type="file" id="imgInput" name="image" accept="image/*">
+                            <input class="rounded-lg" type="file" id="imgInput" name="image" accept="image/*">
                         </div>
                     </div>
                     <br>
                     <button type='submit'
-                        class="inline-flex mx-auto  items-center font-bold px-4 py-2 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 text-white rounded-lg">
-                        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white">
+                        class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+                        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                         </svg>
-                        Save
+                        <span>Save</span>
                     </button>
                 </div>
-                <div class="w-full px-3 sm:w-1/2">
-                    <div class="mb-5">
-                        <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
-                            Email
-                        </label>
-                        <input required type="text" name="email" id="email" value="{{ $user->email }}" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
-                    </div>
-                </div>
-                <div class="w-full px-3 sm:w-1/2">
-                    <div class="mb-5">
-                        <label for="username" class="mb-3 block text-base font-medium text-[#07074D]">
-                            New Username
-                        </label>
-                        <input required type="text" name="username" id="username" value="{{ $user->username }}" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
-                    </div>
-                </div>
-                <div class="w-full px-3 sm:w-1/2">
-                    <div class="mb-5">
-                        <label for="username" class="mb-3 block text-base font-medium text-[#07074D]">
-                            New Profile Photo
-                        </label>
-                        <input class="rounded-lg" type="file" id="imgInput" name="image" accept="image/*">
-                    </div>
-                </div>
-                <br>
-                <button type='submit' class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
-                    <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                    </svg>
-                    <span>Save</span>
-                </button>
-            </div>
-        </form>
-
+            </form>
+        
     </div>
-    @endsection
+@endsection
