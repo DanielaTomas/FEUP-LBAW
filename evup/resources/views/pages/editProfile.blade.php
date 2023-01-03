d1d2b05a4d1894d21e252f4619ad7f0ad99d8839
José Miguel Isidro <up202006485@up.pt>
2023-01-03 16:46:42 +0000

    improved edit profile css

--- evup/resources/views/pages/editProfile.blade.php
+++ evup/resources/views/pages/editProfile.blade.php
@@ -3,9 +3,8 @@
 @section('title', '- Edit Profile')
 
 @section('content')
-    <!-- component -->
-    <h1>Edit My Information</h1>
-    <br>
+
+    <h1 class="mb-4 text-4xl text-center font-bold leading-none tracking-tight text-gray-800">Edit My Information</h1>
     <div class="flex items-center justify-center p-12">
 
         <div class="mx-auto w-full max-w-[550px]">
@@ -31,7 +30,7 @@
                     <div class="w-full px-3 sm:w-1/2">
                         <div class="mb-5">
                             <label for="username" class="mb-3 block text-base font-medium text-[#07074D]">
-                                New Username
+                                Username
                             </label>
                             <input required type="text" name="username" id="username" value="{{ $user->username }}" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
                         </div>
@@ -39,18 +38,18 @@
                     <div class="w-full px-3 sm:w-1/2">
                         <div class="mb-5">
                             <label for="username" class="mb-3 block text-base font-medium text-[#07074D]">
-                                New Profile Photo
+                                Profile Photo
                             </label>
-                            <input class="rounded-lg" type="file" id="imgInput" name="image" accept="image/*">
+                            <input class="rounded-md bg-gray-300" type="file" id="imgInput" name="image" accept="image/*">
                         </div>
                     </div>
                     <br>
                     <button type='submit'
-                        class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
-                        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        class="inline-flex mx-auto  items-center font-bold px-4 py-2 right-2.5 bottom-2.5 bg-gray-900 hover:bg-indigo-600 transition ease-in-out duration-300 text-white rounded-lg">
+                        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white">
                             <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                         </svg>
-                        <span>Save</span>
+                        Save
                     </button>
                 </div>
             </form>
